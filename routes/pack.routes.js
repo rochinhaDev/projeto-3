f5b7148080363d9873304db3fe6b77c9baf264c2
Victor Pimenta <victor.usdevcontact@gmail.com>
2023-09-06 21:00:48 -0400

    pack done 50

--- routes/pack.routes.js
+++ routes/pack.routes.js
@@ -19,6 +19,40 @@ packRouter.post("/create-pack", isAuth, async (req, res) => {
    }
 });
 
+packRouter.get("/all", async (req, res) => {
+    try {
+      const allPacks = await packModel.find({});
+      return res.status(200).json(allPacks);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  });
+
+  packRouter.get("/get-wine/:id", async (req, res) => {
+    try {
+      const id_pack = req.params.id;
+      const pack = await packModel.findById(id_pack);
+      return res.status(200).json(pack);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  });
+
+  packRouter.put("/update/:id"),
+  async (req, res) => {
+    try {
+      const id_pack = req.params.id;
+      const data = req.body;
+      const config = { new: true, runValidators: true };
+      const pack = await packModel.findByIdAndUpdate(id_pack, data, config);
+      return res.status(200).json(pack);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  };
 
 
 
