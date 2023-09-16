38a07d979f4b48679f393941aed89d8dab3d7a64
Victor Pimenta <victor.usdevcontact@gmail.com>
2023-09-16 16:47:39 -0400

    packs change

--- model/pack.model.js
+++ model/pack.model.js
@@ -20,6 +20,7 @@ const packSchema = new Schema(
     delivery: { type: String, enum: ["Pick up", "Delivery"], required: true },
     origin: { type: String, required: true },
     price: { type: String, required: true },
+    photo: { type: String, default: "https://www.packagingsource.com/resize/Shared/Images/Product/Open-Style-Wine-Bottle-Carrier-Kraft-4-Bottle/open-style-wine-packaging.jpg?bw=1000&w=1000&bh=1000&h=1000"},
   },
   { timestamps: true }
 );
